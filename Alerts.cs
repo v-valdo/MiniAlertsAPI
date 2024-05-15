4dd916185c1576aa16042051b6dc6cd151a38efe
v-valdo <v.sersam@gmail.com>
2024-05-15 21:53:08 +0200

    sqlite connection established and basic get for alerts

--- Alerts.cs
+++ Alerts.cs
@@ -1,15 +1,15 @@
 ﻿namespace MiniApi;
+using Microsoft.Data.Sqlite;
 
 public static class Alerts
 {
-    public record AllDTO(int Id, string Name, Severity severity, string description);
+    public record AllDTO(int Id, string Name, string description, Severity severity);
     public record PostDTO(string Name, Severity severity, string description);
 
-    public static List<AllDTO> All(State state)
+    public static List<AllDTO> All(SqliteConnection db)
     {
         List<AllDTO> result = new();
-        state.db.Open();
-        var cmd = state.db.CreateCommand();
+        var cmd = db.CreateCommand();
         cmd.CommandText = "select * from alerts;";
         using var reader = cmd.ExecuteReader();
 
@@ -17,11 +17,11 @@ public static class Alerts
         {
             int id = reader.GetInt32(0);
             string name = reader.GetString(1);
-            string severityText = reader.GetString(2);
+            string description = reader.GetString(2);
+            string severityText = reader.GetString(3);
             Severity severity = Enum.Parse<Severity>(severityText, ignoreCase: true);
-            string description = reader.GetString(3);
 
-            result.Add(new(id, name, severity, description));
+            result.Add(new(id, name, description, severity));
         }
         return result;
     }
--- Program.cs
+++ Program.cs
@@ -1,23 +1,14 @@
 using Microsoft.Data.Sqlite;
+using MiniApi;
 
-var db = new SqliteConnection("Data source=_db");
+await using var db = new SqliteConnection("Data source=_db.db");
+db.Open();
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton(db);
 var app = builder.Build();
 
-app.MapGet("/", () => "Mini-Alert API");
-
 // retrieve a list of alerts
-app.MapGet("/alerts", () => "Mini-Alert API");
-// create new alert
-app.MapPost("/alert", (alert) =>
-{
-    Results.Ok(new Alert(alert.Name, alert.severity, alert.description));
-});
+app.MapGet("/alerts", Alerts.All);
 
-
-app.Run();
-public record (string Name, Severity severity, string description);
-
-public record State(SqliteConnection db);
+app.Run("http://localhost:3002");
